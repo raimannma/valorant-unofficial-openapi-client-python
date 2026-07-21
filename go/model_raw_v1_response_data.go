e5d30e06221ff5639931ba6a978fc2180fa45c25
raimannma <26171511+raimannma@users.noreply.github.com>
2026-07-21 02:48:29 +0000

    chore: Automated API client updates

--- go/model_raw_v1_response_data.go
+++ go/model_raw_v1_response_data.go
@@ -89,7 +89,16 @@ func (dst *RawV1ResponseData) UnmarshalJSON(data []byte) error {
 	} else if match == 1 {
 		return nil // exactly one match
 	} else { // no match
-		return fmt.Errorf("data failed to match schemas in oneOf(RawV1ResponseData)")
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(RawV1ResponseData): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(RawV1ResponseData)")
+        }
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(RawV1ResponseData): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(RawV1ResponseData)")
+        }
 	}
 }
 
